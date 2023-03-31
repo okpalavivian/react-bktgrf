adc5d491584c50592154f1c63668075fdbdc8796
okpalavivian <109100020+okpalavivian@users.noreply.github.com>
2023-03-31 18:34:37 +0100

    1 file modified, 1 added, 1 deleted

--- src/App.js
+++ src/App.js
@@ -1,11 +1,73 @@
-import React from "react";
-import "./style.css";
-
-export default function App() {
+import * as React from 'react';
+import {
+  createBrowserRouter,
+  RouterProvider,
+  Route,
+  Outlet,
+} from 'react-router-dom';
+import Register from './pages/Register';
+import Login from './pages/Login';
+import Single from './pages/Single';
+import Write from './pages/Write';
+import Navbar from './components/Navbar';
+import Footer from './components/Footer';
+import Home from './pages/Home';
+import './style.scss';
+const Layout = () => {
   return (
-    <div>
-      <h1>Hello StackBlitz!</h1>
-      <p>Start editing to see some magic happen :)</p>
+    <>
+      <Navbar />
+      <Outlet />
+      <Footer />
+    </>
+  );
+};
+
+const router = createBrowserRouter([
+  {
+    path: '/',
+    element: <Layout />,
+    children: [
+      {
+        path: '/',
+        element: <Home />,
+      },
+      {
+        path: '/post/id',
+        element: <Single />,
+      },
+      {
+        path: '/',
+        element: <Write />,
+      },
+    ],
+  },
+
+  {
+    path: '/register',
+    element: <Register />,
+  },
+  {
+    path: '/login',
+    element: <Login />,
+  },
+  {
+    path: '/single',
+    element: <Single />,
+  },
+  {
+    path: '/write',
+    element: <Write />,
+  },
+]);
+function App() {
+  return (
+    <div className="app">
+      <div className="container">
+        <RouterProvider router={router} />
+      </div>
     </div>
   );
 }
+
+export default App;
