b1d2286b54df0844bdb40bbe3e3a388605cc4f69
okpalavivian <109100020+okpalavivian@users.noreply.github.com>
2023-03-31 18:32:04 +0100

    4 files added

---
+++ src/components/Footer.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+
+const Footer = () => {
+  return (
+    <footer>
+      <img src="https://i.postimg.cc/gjZzKtJy/Group-3-1.png" alt="" />
+      <span>
+        Made with ♥️ and <b>React.js</b>.
+      </span>
+    </footer>
+  );
+};
+
+export default Footer;
---
+++ src/pages/Login.jsx
@@ -0,0 +1,29 @@
+import axios from 'axios';
+import React, { useState } from 'react';
+import { useContext } from 'react';
+import { Link, useNavigate } from 'react-router-dom';
+import { AuthContext } from '../context/authContext';
+
+const Login = () => {
+  return (
+    <div className="auth">
+      <h1>Login</h1>
+      <form>
+        <input required type="text" placeholder="username" name="username" />
+        <input
+          required
+          type="password"
+          placeholder="password"
+          name="password"
+        />
+        <button>Login</button>
+
+        <span>
+          Don't you have an account? <Link to="/register">Register</Link>
+        </span>
+      </form>
+    </div>
+  );
+};
+
+export default Login;
